9b2e9d4d7bc08b27ffa71a0ba3f89894ea97fa26
Brian Dilts <brianedilts@gmail.com>
2014-07-15 13:55:33 -0600

    Added click handler for all friends

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
 $(document).on('ready', function() {
-  
-$(document).on('click', '.location-box', function(){
+  var colors = ['#2ecc71','#8e44ad','#e74c3c','#f1c40f','#1abc9c','#c0392b'];                
+  var rand = function() {
+   return Math.floor(Math.random()*colors.length);     
+	  }     
+
+  $('.box1').css("background-color", colors[rand()]);
+  $('.box2').css("background-color", colors[rand()]);
+  $('.box3').css("background-color", colors[rand()]);
+  $('.box4').css("background-color", colors[rand()]);
+  $('.box5').css("background-color", colors[rand()]);
+
+
+
+$(document).on('click', '.friend-box', function(event){
+	event.stopPropagation();
 	$(this).addClass('box-a').removeClass('location-box')
+	$(this).find('h1').addClass('reset-friend-name').removeClass('friend-name')
 	$(this).find('img').addClass('img-resizer').removeClass('img-sizer')
-	$(this).append('<div class="share-info"><div class="input-group"><span class="input-group-addon"><input type="checkbox"></span><input type="text" class="form-control" placeholder="Facebook"></div><!-- /input-group --></div>')
+	$(this).append('<div class="share-buttons"><button type="button" class="btn btn-default active top-margin"><i class="fa fa-phone-square">    Phone</i></button><button type="button" class="btn btn-default active top-margin"><i class="fa fa-envelope">    E-mail</i></button><button type="button" class="btn btn-default active top-margin"><i class="fa fa-twitter-square">   Twitter</i></button><button type="button" class="btn btn-default active top-margin"><i class="fa fa-instagram">   Instagram</i></button><button type="button" class="btn btn-default active top-margin" ><i class="fa fa-facebook-square">   Facebook</i></button><button type="button" id="loading-example-btn" data-loading-text="Loading..." class="btn btn-default active top-margin"><i class="fa fa-linkedin-square">   Linkedin</i></button><textarea class="form-control top-margin" rows="3" placeholder="Leave this person a message"></textarea><div class="share-items top-margin"><button type="button" class="btn btn-danger btn-lg btn-block">Share these items</button></div></div>')
 	// $('location-box img:first-child').addClass('img-resizer')
 	// $('location-box img:first-child').removeClass('img-sizer')
 })
@@ -13,4 +27,10 @@ $(document).on('click', '.location-box', function(){
 
 });
 
-
+  // $('#loading-example-btn').click(function () {
+  //   var btn = $(this)
+  //   btn.button('loading')
+  //   $.ajax(...).always(function () {
+  //     btn.button('reset')
+  //   });
+  // });
