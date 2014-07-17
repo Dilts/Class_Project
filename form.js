0d6f6350ebaab9f40158015d4657279fe8cde8c4
Brian Dilts <brianedilts@gmail.com>
2014-07-17 09:49:13 -0600

    first push from home

--- form.js
+++ form.js
@@ -2,30 +2,32 @@ $(document).on('ready', function() {
 
 
 
-var Friend = function () {
+
+
+// var Friend = function () {
 	
 	
-}
+// }
 
-Friend.prototype.create = function() {
-	this.element = $('<div></div>')
-		.addClass('newbox');
-		return this.element;
-};
+// Friend.prototype.create = function() {
+// 	this.element = $('<div></div>')
+// 		.addClass('signup-box');
+// 		return this.element;
+// };
 
-var newFriend = new Friend();
-$('body').append(newFriend.create());
+// var newFriend = new Friend();
+// $('body').append(newFriend.create());
 
 
-var Form = function () {
+// var Form = function () {
 
-}
-Form.prototype.create = function() {
-	this.el = $()
-	return this.el
-};
-var newForm = new Form();
-$('body').append(newForm.create());
+// }
+// Form.prototype.create = function() {
+// 	this.el = $()
+// 	return this.el
+// };
+// var newForm = new Form();
+// $('body').append(newForm.create());
 
 
 });
--- main.js
+++ main.js
@@ -3,7 +3,6 @@ $(document).on('ready', function() {
   var colorChange = colors.slice(); 
 
 
-
   var rand = function() {
    return Math.floor(Math.random()*colors.length);     
 	  }     
@@ -14,15 +13,21 @@ $(document).on('ready', function() {
   $('.box4').css("background-color", colors[rand()]);
   $('.box5').css("background-color", colors[rand()]);
 
+// $('.btn').button('toggle');
+// $("a").hover(function(){
+//   $("a").css("background-color","yellow");
+// });
 
 
 $(document).on('click', '.friend-box', function(event){
 	event.stopPropagation();
-	$(this).addClass('box-a').removeClass('location-box')
+	$(this).addClass('box-a').removeClass('friend-box')
 	$(this).find('h1').addClass('reset-friend-name').removeClass('friend-name')
 	$(this).find('img').addClass('img-resizer').removeClass('img-sizer')
-	$(this).append('<div class="share-buttons"><button type="button" class="btn btn-default active top-margin"><i class="fa fa-phone-square">    Phone</i></button><button type="button" class="btn btn-default active top-margin"><i class="fa fa-envelope">    E-mail</i></button><button type="button" class="btn btn-default active top-margin"><i class="fa fa-twitter-square">   Twitter</i></button><button type="button" class="btn btn-default active top-margin"><i class="fa fa-instagram">   Instagram</i></button><button type="button" class="btn btn-default active top-margin" ><i class="fa fa-facebook-square">   Facebook</i></button><button type="button" id="loading-example-btn" data-loading-text="Loading..." class="btn btn-default active top-margin"><i class="fa fa-linkedin-square">   Linkedin</i></button><textarea class="form-control top-margin" rows="3" placeholder="Leave this person a message"></textarea><div class="share-items top-margin"><button type="button" class="btn btn-info btn-lg btn-block">Share these items</button></div></div>')
+	$(this).append('<div class="share-buttons"><button type="button" class="btn btn-default active top-margin" data-toggle="button"><i class="fa fa-phone-square">    Phone</i></button><button type="button" class="btn btn-default active top-margin"><i class="fa fa-envelope">    E-mail</i></button><button type="button" class="btn btn-default active top-margin"><i class="fa fa-twitter-square">   Twitter</i></button><button type="button" class="btn btn-default active top-margin"><i class="fa fa-instagram">   Instagram</i></button><button type="button" class="btn btn-default active top-margin" ><i class="fa fa-facebook-square">   Facebook</i></button><button type="button" id="loading-example-btn" data-loading-text="Loading..." class="btn btn-default active top-margin"><i class="fa fa-linkedin-square">   Linkedin</i></button><textarea class="form-control top-margin" rows="3" placeholder="Leave this person a message"></textarea><div class="share-items top-margin"><button type="button" class="btn btn-info btn-lg btn-block">Share these items</button></div></div>')
 })
+
+
 
 
 
