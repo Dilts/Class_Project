cb35fb41b3475786b21bd278f79bf6dc1d4ac077
Brian Dilts <brianedilts@gmail.com>
2014-07-18 11:00:52 -0600

    last push before presentation

--- form.js
+++ form.js
@@ -1,11 +1,79 @@
 $(document).on('ready', function() {
 
 
+// Create an array of obj with search terms
+var info = [];
 
 
-
-// var Friend = function () {
+$('.submit7').on('click', function( event ){
+	event.preventDefault();
+	var obj = {
+		name: $('#name').val(),
+		location: $('#location').val(),
+	}
+	info.push(obj);
+	console.log(info)
+})
 	
+$('.sub').on('click', function( event ){
+	event.preventDefault();
+	var obj = {			
+		friendEmail: $('#friend').val()
+	}
+	info.push(obj);
+	console.log(info)
+	})
+		
+
+// buttons list in console. beginning of share array
+var sharethis = [];
+
+sharethis.push($(this).data('share-type'));
+
+
+
+// Signup new user
+
+var newUser = [];
+
+$('.signup').on('click', function( event ){
+	event.preventDefault();
+	var arr = [];
+
+
+	// loop
+
+	var input = {type: 
+				 available: 
+				 value:  }
+// set values for input
+				 arr.push(input)
+				 // end loop
+	var newSignup = {
+		firstName: $('#first-name').val(),
+		lastName: $('#last-name').val(),
+		email: $('#enter-email').val(),
+		password: $('#password').val(),
+		inputs: arr
+
+	newUser.push(newSignup);
+	console.log(newUser)
+})
+// 
+
+
+// $( "input[type='checkbox'], input[type='radio']" ).on( "click", showValues );
+
+
+
+});
+ 
+
+
+
+
+
+// var Friend = function () {	
 	
 // }
 
@@ -30,5 +98,4 @@ $(document).on('ready', function() {
 // $('body').append(newForm.create());
 
 
-});
 
--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 $(document).on('ready', function() {
+ 
   var colors = ['#2ecc71','#8e44ad','#e74c3c','#f1c40f','#1abc9c','#c0392b'];
   var colorChange = colors.slice(); 
 
@@ -19,20 +20,33 @@ $(document).on('ready', function() {
 // });
 
 
+
 $(document).on('click', '.friend-box', function(event){
 	event.stopPropagation();
 	$(this).addClass('box-a').removeClass('friend-box')
 	$(this).find('h1').addClass('reset-friend-name').removeClass('friend-name')
 	$(this).find('img').addClass('img-resizer').removeClass('img-sizer')
-	$(this).append('<div class="share-buttons"><button type="button" class="btn btn-default active top-margin" data-toggle="button"><i class="fa fa-phone-square">    Phone</i></button><button type="button" class="btn btn-default active top-margin"><i class="fa fa-envelope">    E-mail</i></button><button type="button" class="btn btn-default active top-margin"><i class="fa fa-twitter-square">   Twitter</i></button><button type="button" class="btn btn-default active top-margin"><i class="fa fa-instagram">   Instagram</i></button><button type="button" class="btn btn-default active top-margin" ><i class="fa fa-facebook-square">   Facebook</i></button><button type="button" id="loading-example-btn" data-loading-text="Loading..." class="btn btn-default active top-margin"><i class="fa fa-linkedin-square">   Linkedin</i></button><textarea class="form-control top-margin" rows="3" placeholder="Leave this person a message"></textarea><div class="share-items top-margin"><button type="button" class="btn btn-info btn-lg btn-block">Share these items</button></div></div>')
+	$(this).append('<div class="share-buttons"><button type="button" data-share-type="phone" class="btn btn-default active top-margin" data-toggle="button"><i class="fa fa-phone-square">    Phone</i></button><button type="button" data-share-type="email" class="btn btn-default active top-margin"><i class="fa fa-envelope">    E-mail</i></button><button type="button" data-share-type="Twitter" class="btn btn-default active top-margin"><i class="fa fa-twitter-square">   Twitter</i></button><button type="button" data-share-type="Instagram" class="btn btn-default active top-margin"><i class="fa fa-instagram">   Instagram</i></button><button type="button" data-share-type="Facebook" class="btn btn-default active top-margin" ><i class="fa fa-facebook-square">   Facebook</i></button><button type="button" data-share-type="Linkedin" class="btn btn-default active top-margin"><i class="fa fa-linkedin-square">   Linkedin</i></button></div><textarea class="form-control top-margin" rows="3" placeholder="Leave this person a message"></textarea><div class="share-items top-margin"><button type="button" class="btn btn-info btn-lg btn-block">Share these items</button></div>')
 })
 
 
+$(document).on('click', '.share-buttons button', function () {
+    $(this).css({'background-color': '#88BCE2'});
 
+
+
+    console.log($(this).data('share-type'));
+
+
+});
 
 
 
 });
+
+// var sharethis = [];
+
+// sharethis.push($(this).data('share-type'));
 
 // attempt 1
 
